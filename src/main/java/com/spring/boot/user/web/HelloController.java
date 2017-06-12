940689407a20e4fd90c4ec97adbdab717b974a74
kevin1168 <526337515@qq.com>
2017-06-12 10:14:11 +0800

    添加集成themeleaf

---
+++ src/main/java/com/spring/boot/user/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.spring.boot.user.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	/**
+	 * 测试hello
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello(Model model) {
+		model.addAttribute("name", "Dear");
+		return "hello";
+	}
+
+}
